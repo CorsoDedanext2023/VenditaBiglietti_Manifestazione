091f3dec2b47c1399a5498e7326fb3427a1118d2
Gabriele De Cataldis <gabriele.decataldis@dedagroup.it>
2023-11-20 16:42:32 +0100

    Aggiorno la fork del microservizio
    aggiunto il file data.sql
    modificate le variabili del model

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+-- Inserimenti per Biglietto
+INSERT INTO Biglietto (dataAcquisto, prezzo, nSeriale, isCancellato, utente_id, prezzoSettoreEvento_id, version) 
+VALUES ('2023-01-01', 30.0, 'ABC123', false, 1, 1, 1);
+INSERT INTO Biglietto (dataAcquisto, prezzo, nSeriale, isCancellato, utente_id, prezzoSettoreEvento_id, version) 
+VALUES ('2023-02-15', 25.5, 'DEF456', false, 2, 2, 1);
+INSERT INTO Biglietto (dataAcquisto, prezzo, nSeriale, isCancellato, utente_id, prezzoSettoreEvento_id, version) 
+VALUES ('2023-03-20', 15.0, 'GHI789', false, 3, 3, 1);
+INSERT INTO Biglietto (dataAcquisto, prezzo, nSeriale, isCancellato, utente_id, prezzoSettoreEvento_id, version) 
+VALUES ('2023-04-10', 20.0, 'JKL012', false, 4, 4, 1);
+INSERT INTO Biglietto (dataAcquisto, prezzo, nSeriale, isCancellato, utente_id, prezzoSettoreEvento_id, version) 
+VALUES ('2023-05-25', 18.5, 'MNO345', false, 5, 5, 1);
+INSERT INTO Biglietto (dataAcquisto, prezzo, nSeriale, isCancellato, utente_id, prezzoSettoreEvento_id, version) 
+VALUES ('2023-06-21', 12.0, 'PQR678', false, 6, 6, 1);
+INSERT INTO Biglietto (dataAcquisto, prezzo, nSeriale, isCancellato, utente_id, prezzoSettoreEvento_id, version) 
+VALUES ('2023-07-31', 25.0, 'STU901', false, 7, 7, 1);
+INSERT INTO Biglietto (dataAcquisto, prezzo, nSeriale, isCancellato, utente_id, prezzoSettoreEvento_id, version) 
+VALUES ('2023-08-15', 22.5, 'VWX234', false, 8, 8, 1);
+INSERT INTO Biglietto (dataAcquisto, prezzo, nSeriale, isCancellato, utente_id, prezzoSettoreEvento_id, version) 
+VALUES ('2023-09-10', 28.0, 'YZA567', false, 9, 9, 1);
+INSERT INTO Biglietto (dataAcquisto, prezzo, nSeriale, isCancellato, utente_id, prezzoSettoreEvento_id, version) 
+VALUES ('2023-10-15', 30.0, 'BCD890', false, 10, 10, 1);
